8b11de3227e724746a4c9ddc4e2861872574d33c
picoliW <leduardo3011@gmail.com>
2024-03-14 11:56:38 -0300

    Achievements system improvements

--- achievements/achievements.py
+++ achievements/achievements.py
@@ -1,26 +1,28 @@
 goblins_killed_goal = 10
 goblins_killed_reward = 25
 dragons_killed_goal = 5
-dragons_killed_reward = 5
-
+dragons_killed_reward = 50
+from characters.player import player_instance
 
 def achievements_progress():
     from messagess.goblin_killed import goblins_killed
     from messagess.dragon_killed import dragons_killed
     global goblins_killed_goal
     global goblins_killed_reward
-
-    if goblins_killed > goblins_killed_goal:
+    
+    if goblins_killed >= goblins_killed_goal:
         goblins_killed = goblins_killed_goal
-    if dragons_killed > dragons_killed_goal:
-        dragons_killed = dragons_killed_goal
-    
-    print(f'Goblins killed {goblins_killed} / {goblins_killed_goal} - Reward: {goblins_killed_reward} gold')
-    
-    
-    print(f'Dragons killed {dragons_killed} / {dragons_killed_goal} - Reward: {dragons_killed_reward}')
+        print(f'Goblin slayer - Goblins killed {goblins_killed} / {goblins_killed_goal} - Reward: {goblins_killed_reward} gold')
+        print(f'Achievement completed!')
+    else:
+        print(f'Goblins killed {goblins_killed} / {goblins_killed_goal} - Reward: {goblins_killed_reward} gold')
 
-    
+    if dragons_killed >= dragons_killed_goal:
+        dragons_killed = dragons_killed_goal
+        print(f'Dragons killed {dragons_killed_goal} / {dragons_killed_goal} - Reward: {dragons_killed_reward}')
+        print(f'Achievement completed!')
+    else:
+        print(f'Dragons killed {dragons_killed} / {dragons_killed_goal} - Reward: {dragons_killed_reward}')
 
     from mainMenu import menu
     menu()
--- messagess/goblin_killed.py
+++ messagess/goblin_killed.py
@@ -3,16 +3,19 @@ from characters.player import player_instance
 
 
 goblins_killed = 0
+achievement_completed = False
 
 def goblin_killed():
-    global goblins_killed
+    from achievements.achievements import goblins_killed_goal
+    from achievements.achievements import goblins_killed_reward
+    global goblins_killed, achievement_completed
     if goblin_instance.health <= 0:
         player_instance.gold += 20
         print('You killed a goblin! +20 gold')
         goblins_killed += 1
         goblin_instance.health = 40
         goblin_instance.armor = 10
-    if goblins_killed >= 10:
-        from achievements.achievements import goblins_killed_goal
-        print(f'Achievement completed! {goblins_killed_goal} gold')
-        player_instance.gold += 25
+    if goblins_killed >= goblins_killed_goal and achievement_completed is False:
+        print(f'Achievement completed! You received {goblins_killed_reward} gold')
+        player_instance.gold += goblins_killed_reward
+        achievement_completed = True
